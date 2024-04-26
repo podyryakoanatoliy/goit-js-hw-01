df7b578654cf9b1c8907c017f04ce18bc894eee0
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2024-04-26 17:01:07 +0300

    ready all task

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    const numContent = Number.parseFloat(content);
+    const numPadding = Number.parseFloat(padding);
+    const numBorder = Number.parseFloat(border);
+    const totalElementWidth = numContent + (2 * numPadding) + (2 * numBorder);
+    return `The total width of the screen is: ${totalElementWidth} px`;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
